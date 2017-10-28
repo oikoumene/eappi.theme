99a05896decfc96445b1abf4ed0a0f21711ce18d
holdenhao <holdenhao@gmail.com>
2017-10-28 16:19:50 +0800

    comment out requirement for paster

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(name='eappi.theme',
       # The next two lines may be deleted after you no longer need
       # addcontent support from paster and before you distribute
       # your package.
-      # setup_requires=["PasteScript"],
-      # paster_plugins=["templer.localcommands"],
+      # #setup_requires=["PasteScript"],
+      # #paster_plugins=["templer.localcommands"],
 
       )
